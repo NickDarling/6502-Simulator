7232df5179927b461689d8070e365f78aba7711f
NickDarling <mrnickdarling@gmail.com>
2015-03-26 14:52:30 -0700

    - finished the following instructions (for all of their addressing modes) - LDA - load accumulator - LDX - load x register - LDY - load y register - STA - store accumulator - STX - store x rgister - STY - store y register - TAX - transfer acc to x - TAY - transfer acc to y - TXA - transfer X to acc - TYA - transfer y to acc - TSX - transfer stack pointer to X - TXS - transfer x to stack pointer

--- CSC350P2.java
+++ CSC350P2.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class CSC350P2 {
 
 	static final short STARTING_MEMORY_LOCATION = 0x0600;
+	static final short STARTING_STACK_LOCATION = 0x0100;	
 	static final int TOTAL_MEMORY = 65536; 	// 4KB
 	
 	public static void main (String [] args) {
@@ -12,11 +13,11 @@ public class CSC350P2 {
 		short PC = STARTING_MEMORY_LOCATION;	// program counter
 		
 		// 8-bit registers
-		byte AC = 0;				// accumulator
-		byte X = 0;					// X register
-		byte Y = 0;					// Y register
-		byte SR = 0;				// status register
-		byte SP = 0; 				// stack pointer
+		byte AC = 0;		// accumulator
+		byte X = 0;			// X register
+		byte Y = 0;			// Y register
+		byte SR = 0;		// status register
+		byte SP = 0; 		// stack pointer - contains next free byte of stack space
 		
 		// initialize flags
 		boolean N = false;		// negative
@@ -32,13 +33,14 @@ public class CSC350P2 {
 		// open assembly code
 		Scanner scanner = null;
 		try {
-			scanner = new Scanner (new File ("program1.txt"));
+			scanner = new Scanner (new File ("program1.asm"));
 		} catch (Exception e) {
 			System.out.println("Error: Couldn't open file");
 			System.exit(0);
 		}
 		
 		byte [] memory = new byte [TOTAL_MEMORY];	// 4 KB of memory
+
 		int currLocation = STARTING_MEMORY_LOCATION;
 
 		// put program in memory
@@ -96,18 +98,41 @@ public class CSC350P2 {
 			// 2. find instruction
 			switch (opcode) {
 			
-			// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * * * 
-			// Load/store operations  
-			// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * * *
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+				// Load/store operations  
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+				// -------------------- LDA - Load Accumulator --------------------
 				case (byte) 0xA9: 	
 					// LDA - Load Accumulator - Immediate
-					data1 =  memory[PC++];
+					data1 = memory[PC++];
 					AC = data1;	
 					
 					// update flags
 					Z = updateZFlag(AC);
 					N = updateNFlag(AC);			
 					break;	
+					
+				case (byte) 0xA5:
+					// LDA - Load Accumulator - Zero Page
+					data1 = memory[PC++];
+					
+					AC = memory[data1];
+					
+					// update flags
+					Z = updateZFlag(AC);
+					N = updateNFlag(AC);	
+					break;	
+					
+				case (byte) 0xB5:
+					// LDA - Load Accumulator - Zero Page, X
+					data1 = memory[PC++];
+					
+					AC = memory[data1 + X];
+					
+					// update flags
+					Z = updateZFlag(AC);
+					N = updateNFlag(AC);	
+					break;				
 					
 				case (byte) 0xAD:
 					// LDA - Load Accumulator - Absolute (16-bit address)
@@ -154,19 +179,269 @@ public class CSC350P2 {
 					// update flags
 					Z = updateZFlag(AC);
 					N = updateNFlag(AC);					
-					break;						
+					break;			
+					
+				case (byte) 0xB1:
+					// LDA - Load Accumulator - (Indirect), Y (16-bit address)
+					data1 = memory[PC++];
+					
+					AC = memory[memory[data1] + Y];
+					
+					// update flags
+					Z = updateZFlag(AC);
+					N = updateNFlag(AC);					
+					break;					
+					
+				// -------------------- LDX - Load X Register  --------------------
+				case (byte) 0xA2:
+					// LDX - Load X Register - Immediate
+					data1 = memory[PC++];
+					X = data1;	
+					
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);			
+					break;	
+					
+				case (byte) 0xA6:
+					// LDX - Load X Register - Zero Page
+					data1 = memory[PC++];		
+					X = memory[data1];
+					
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);	
+					break;	
+					
+				case (byte) 0xB6:
+					// LDX - Load X Register - Zero Page, Y
+					data1 = memory[PC++];
+					X = memory[data1 + Y];
+					
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);	
+					break;	
+					
+				case (byte) 0xAE:
+					// LDX - Load X Register - Absolute (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					
+					X = memory[twoBytesToShort(data1, data2)];
+					
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);	
+					break;
+					
+				case (byte) 0xBE:
+					// LDX - Load X Register - Absolute,Y (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					
+					X = memory[Y + twoBytesToShort(data1, data2)];
+					
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);					
+					break;	
+				
+				// -------------------- LDY - Load Y Register  --------------------	
+				case (byte) 0xA0:
+					// LDY - Load Y Register - Immediate
+					data1 = memory[PC++];
+					Y = data1;	
+					
+					// update flags
+					Z = updateZFlag(Y);
+					N = updateNFlag(Y);			
+					break;	
+					
+				case (byte) 0xA4:
+					// LDY - Load Y Register - Zero Page
+					data1 = memory[PC++];		
+					Y = memory[data1];
+					
+					// update flags
+					Z = updateZFlag(Y);
+					N = updateNFlag(Y);	
+					break;	
+					
+				case (byte) 0xB4:
+					// LDY - Load X Register - Zero Page, Y
+					data1 = memory[PC++];
+					Y = memory[data1 + X];
+					
+					// update flags
+					Z = updateZFlag(Y);
+					N = updateNFlag(Y);	
+					break;	
+					
+				case (byte) 0xAC:
+					// LDY - Load Y Register - Absolute (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					
+					Y = memory[twoBytesToShort(data1, data2)];
+					
+					// update flags
+					Z = updateZFlag(Y);
+					N = updateNFlag(Y);	
+					break;
+					
+				case (byte) 0xBC:
+					// LDY - Load Y Register - Absolute,X (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					
+					Y = memory[X + twoBytesToShort(data1, data2)];
+					
+					// update flags
+					Z = updateZFlag(Y);
+					N = updateNFlag(Y);					
+					break;	
+					
+				// -------------------- STA - Store Accumulator  --------------------							
+				case (byte) 0x85:
+					// STA - Store Accumulator - Zero Page
+					data1 = memory[PC++];
+					memory[data1] = AC;
+					break;	
+					
+				case (byte) 0x95:
+					// STA - Store Accumulator - Zero Page, X
+					data1 = memory[PC++];
+					memory[data1 + X] = AC;
+					break;				
 
 				case (byte) 0x8D:
 					// STA - Store Accumulator - Absolute (16-bit address)
 					data1 = memory[PC++];
 					data2 = memory[PC++];
-					
 					memory[twoBytesToShort(data1, data2)] = AC;
+					break;					
+					
+				case (byte) 0x9D:
+					// STA - Store Accumulator - Absolute,X (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];		
+					memory[X + twoBytesToShort(data1, data2)] = AC;			
 					break;
+					
+				case (byte) 0x99:
+					// STA - Store Accumulator - Absolute,Y (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					memory[Y + twoBytesToShort(data1, data2)] = AC;				
+					break;	
+					
+				case (byte) 0x81:
+					// STA - Store Accumulator - (Indirect,X) (16-bit address)
+					data1 = memory[PC++];
+					memory[memory[X + data1]] = AC;				
+					break;			
+					
+				case (byte) 0x91:
+					// STA - Store Accumulator - (Indirect), Y (16-bit address)
+					data1 = memory[PC++];
+					memory[memory[data1] + Y] = AC;				
+					break;								
+			
+				// -------------------- STX - Store X Register  --------------------							
+				case (byte) 0x86:
+					// STX - Store X Register - Zero Page
+					data1 = memory[PC++];
+					memory[data1] = X;
+					break;	
+					
+				case (byte) 0x96:
+					// STX - Store X Register - Zero Page, Y
+					data1 = memory[PC++];
+					memory[data1 + Y] = X;
+					break;	
+					
+				case (byte) 0x8E:
+					// STX - Store X Register - Absolute (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					memory[twoBytesToShort(data1, data2)] = X;
+					break;					
+					
+				// -------------------- STY - Store Y Register  --------------------							
+				case (byte) 0x84:
+					// STY - Store Y Register - Zero Page
+					data1 = memory[PC++];
+					memory[data1] = Y;
+					break;	
+					
+				case (byte) 0x94:
+					// STY - Store Y Register - Zero Page, X
+					data1 = memory[PC++];
+					memory[data1 + X] = Y;
+					break;	
+					
+				case (byte) 0x8C:
+					// STY - Store Y Register - Absolute (16-bit address)
+					data1 = memory[PC++];
+					data2 = memory[PC++];
+					memory[twoBytesToShort(data1, data2)] = Y;
+					break;							
+		
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+				// Register Transfer Operations 
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *										
+				// -------------------- TAX - Transfer Accumulator to X  --------------------							
+				case (byte) 0xAA:
+					X = AC;	
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);					
+					break;	
+					
+				// -------------------- TAY - Transfer Accumulator to Y  --------------------							
+				case (byte) 0xA8:
+					Y = AC;	
+					// update flags
+					Z = updateZFlag(Y);
+					N = updateNFlag(Y);					
+					break;						
 	
-				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * * * 
+				// -------------------- TXA - Transfer X to Accumulator --------------------							
+				case (byte) 0x8A:
+					AC = X;	
+					// update flags
+					Z = updateZFlag(AC);
+					N = updateNFlag(AC);					
+					break;
+					
+				// -------------------- TYA - Transfer Y to Accumulator --------------------							
+				case (byte) 0x98:
+					AC = Y;	
+					// update flags
+					Z = updateZFlag(AC);
+					N = updateNFlag(AC);					
+					break;		
+					
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+				// Stack Operations 
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *						
+				// -------------------- TSX - Transfer Stack Pointer to X --------------------	
+				case (byte) 0xBA:
+					X = SP;
+					// update flags
+					Z = updateZFlag(X);
+					N = updateNFlag(X);	
+					break;
+				
+				// -------------------- TXS - Transfer X to Stack Pointer --------------------	
+				case (byte) 0x9A:
+					SP = X;
+					break;
+						
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 				// Logical operations 
-				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * * *					
+				// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *					
 				case (byte) 0x29:
 					// AND - Logical And - immediate
 					data1 =  memory[PC++];
@@ -226,7 +501,7 @@ public class CSC350P2 {
 			// 4. wait for button press?			
 		}
 	}
-	
+
 	// take string in base 16 and return byte
 	public static byte strToByte(String str) {
 		return (byte)Integer.parseInt(str, 16);
